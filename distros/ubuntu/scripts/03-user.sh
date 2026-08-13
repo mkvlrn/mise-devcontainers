d22a0f1bb0e820d16f19326fd245f5744cbfcc54
mkvlrn <mkvlrn@gmail.com>
2026-08-13 19:28:42 -0300

    fix: fix user script for ubuntu

---
+++ distros/ubuntu/scripts/03-user.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+
+# set defaults
+USERNAME="dev"
+USER_UID="1000"
+USER_GID="$USER_UID"
+
+# remove default ubuntu user
+OLD_USER="$(getent passwd "$USER_UID" | cut -d: -f1)"
+userdel -r "$OLD_USER"
+
+# remove default ubuntu group
+OLD_GROUP="$(getent group "$USER_GID" | cut -d: -f1)"
+groupdel "$OLD_GROUP"
+
+# create dev user
+groupadd -g "$USER_GID" "$USERNAME"
+useradd -m -s /bin/fish -u "$USER_UID" -g "$USER_GID" "$USERNAME"
+usermod -aG docker "$USERNAME"
+
+# configure passwordless sudo
+echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" >"/etc/sudoers.d/$USERNAME"
+chmod 0440 "/etc/sudoers.d/$USERNAME"
+
+# prepare .ssh
+mkdir -p "/home/$USERNAME/.ssh"
+chmod 700 "/home/$USERNAME/.ssh"
+
+# set ownership of home
+chown -R "$USERNAME:$USERNAME" "/home/$USERNAME"
