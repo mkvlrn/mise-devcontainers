0390fd2fb9b570e7708da024471d2fea9808a23a
mkvlrn <mkvlrn@gmail.com>
2026-08-16 06:01:34 -0300

    fix: ssh testing

--- src/_common/binscripts/post-start.sh
+++ src/_common/binscripts/post-start.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+# ssh setup
+mkdir -p /home/dev/.ssh
+ssh-add -L >/home/dev/.ssh/authorized_keys
+chown -R dev:dev /home/dev/.ssh
+chmod 700 /home/dev/.ssh
+chmod 600 /home/dev/.ssh/authorized_keys
+
 # mise lifecycle
 mise self-update -y || true
 mise cache clear || true
