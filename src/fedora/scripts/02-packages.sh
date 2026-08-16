82bc8ef288e6bce0a5a1287e7ac8bfee7fdab5aa
mkvlrn <mkvlrn@gmail.com>
2026-08-16 07:17:26 -0300

    fix: ssh testing

--- src/fedora/scripts/02-packages.sh
+++ src/fedora/scripts/02-packages.sh
@@ -3,22 +3,25 @@ set -e
 
 # install base tools for a working system
 dnf install -y \
-    curl \
-    docker-buildx \
-    docker-cli \
-    docker-compose \
-    fish \
-    git \
-    htop \
-    less \
-    moby-engine \
-    openssh-clients \
-    openssh-server \
-    shadow-utils \
-    sudo \
-    tzdata \
-    iptables-nft \
-    runc
+  curl \
+  docker-buildx \
+  docker-cli \
+  docker-compose \
+  fish \
+  git \
+  htop \
+  less \
+  moby-engine \
+  openssh-clients \
+  openssh-server \
+  shadow-utils \
+  sudo \
+  tzdata \
+  iptables-nft \
+  runc
+
+# disable PAM for sshd
+sed -i 's/^UsePAM yes$/UsePAM no/' /etc/ssh/sshd_config.d/50-redhat.conf
 
 # prioritize nft iptables for dockerd
 alternatives --set iptables /usr/bin/iptables-nft
