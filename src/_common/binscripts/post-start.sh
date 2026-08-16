e5631e2b3cbac2494586bafef600dca302c0bc88
mkvlrn <mkvlrn@gmail.com>
2026-08-16 06:05:25 -0300

    fix: ssh testing

--- src/_common/binscripts/post-start.sh
+++ src/_common/binscripts/post-start.sh
@@ -4,7 +4,8 @@ set -e
 # ssh setup
 mkdir -p /home/dev/.ssh
 ssh-add -L >/home/dev/.ssh/authorized_keys
-chown -R dev:dev /home/dev/.ssh
+chown dev:dev /home/dev/.ssh
+chown dev:dev /home/dev/.ssh/authorized_keys
 chmod 700 /home/dev/.ssh
 chmod 600 /home/dev/.ssh/authorized_keys
 
