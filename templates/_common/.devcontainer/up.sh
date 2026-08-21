ebd94592e1c3438f5ec425a43f811832b1473ed7
mkvlrn <mkvlrn@gmail.com>
2026-08-21 20:39:26 -0300

    fix: ssh homedir

--- templates/_common/.devcontainer/up.sh
+++ templates/_common/.devcontainer/up.sh
@@ -120,7 +120,7 @@ Host $SSH_TARGET
     User dev
     StrictHostKeyChecking no
     UserKnownHostsFile /dev/null
-Match host $SSH_TARGET command ""
+Match host $SSH_TARGET exec "test -t 0"
     RemoteCommand cd /code/$PROJECT && exec "\$SHELL" -l
     RequestTTY force
 EOF
