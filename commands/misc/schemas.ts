b3f697953bed96f7fd07ddd9f94667b40c4199ba
mkvlrn <dev@mkvlrn.cc>
2026-08-22 14:55:49 -0300

    ci: temp dispatch

--- commands/misc/schemas.ts
+++ commands/misc/schemas.ts
@@ -61,4 +61,5 @@ export const envSchema = z.object({
   BUILD_IMAGE_RESULT: z.enum(["success", "failure", "cancelled", "skipped"]).optional(),
   CREATE_TEMPLATE_RESULT: z.enum(["success", "failure", "cancelled", "skipped"]).optional(),
   TEST_TEMPLATE_RESULT: z.enum(["success", "failure", "cancelled", "skipped"]).optional(),
+  INPUT_HEAD_SHA: z.string().optional(),
 });
