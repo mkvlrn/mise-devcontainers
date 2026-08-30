63ff88e75f7d7b6d4a987923913fb65046df6c72
mkvlrn <dev@mkvlrn.cc>
2026-08-30 10:39:02 -0300

    fix: fish config, and completions

--- distros/_common/.config/fish/conf.d/00-env.fish
+++
@@ -1,6 +0,0 @@
-# paths
-set -gx HOME_BIN "$HOME/.local/bin"
-set -gx USR_LOCAL_BIN /usr/local/bin
-
-# PATH
-fish_add_path --prepend $HOME_BIN $USR_LOCAL_BIN
--- distros/_common/.config/fish/conf.d/01-sources.fish
+++
@@ -1,5 +0,0 @@
-# mise
-mise activate fish | source
-
-# oh-my-posh
-oh-my-posh init fish --config "https://raw.githubusercontent.com/mkvlrn/ts-tools/main/packages/config/src/mkvlrn.omp.jsonc" | source
