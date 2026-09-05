87dec49a0d9d84e685a8286ca02f4ffa8d7e02cc
mkvlrn <dev@mkvlrn.cc>
2026-09-05 19:37:30 -0300

    fix: move needed envs to base dockerfile (#44)

--- distros/_common/.config/fish/config.fish
+++ distros/_common/.config/fish/config.fish
@@ -1,20 +1,8 @@
 # no greeting
 set -g fish_greeting
 
-# initially for crontab, maybe others, idk
-set -gx VISUAL "code --wait"
-set -gx EDITOR "code --wait"
-
-# paths
-set -g HOME_BIN "$HOME/.local/bin"
-set -g USR_LOCAL_BIN /usr/local/bin
-set -gx GOPATH "$HOME/.go"
-
-# PATH
-fish_add_path --prepend $HOME_BIN $USR_LOCAL_BIN $GOPATH/bin
-
 # mise
-mise activate fish | source
+$HOME/.local/bin/mise activate fish | source
 
 # interactive
 if status is-interactive
